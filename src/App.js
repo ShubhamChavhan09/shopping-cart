4aefd3de31f51bd25809295291c14a9cf8024618
Shubham <shubh.chavhan00@gmail.com>
2021-11-29 20:11:53 +0530

    Wrapped SideBar and Routes in a Div

--- src/App.js
+++ src/App.js
@@ -3,15 +3,24 @@ import Header from "./components/Header.js";
 import SideBar from "./components/SideBar/index.js";
 import GlobalStyle from "./theme/globalStyle.js";
 import { BrowserRouter } from "react-router-dom";
+import Routes from "./routes/index.js";
+import styled from "styled-components";
 
 function App() {
   return (
     <BrowserRouter>
       <GlobalStyle />
       <Header />
-      <SideBar />
+      <Main>
+        <SideBar />
+        <Routes />
+      </Main>
     </BrowserRouter>
   );
 }
 
 export default App;
+
+const Main = styled.div`
+  display: flex;
+`;
