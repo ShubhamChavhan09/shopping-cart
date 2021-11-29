d76b66ed268903328ad87fed6e3877c39017bcee
Shubham <shubh.chavhan00@gmail.com>
2021-11-29 20:51:58 +0530

    Added data from faker inside Context to be accessed all over the app

--- src/context/Context.js
+++ src/context/Context.js
@@ -1,8 +1,18 @@
 import { createContext } from "react";
+import faker from "faker";
 
 const Cart = createContext();
 
 const Context = ({ children }) => {
+  const products = [...Array(20)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),
+  }));
+
+  console.log(products);
+
   return <Cart.Provider>{children}</Cart.Provider>;
 };
 
