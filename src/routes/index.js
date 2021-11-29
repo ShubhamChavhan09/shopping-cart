64e2b7a446187b9c85ad8f3915473dcd1043fc1b
Shubham <shubh.chavhan00@gmail.com>
2021-11-29 20:10:49 +0530

    Routes: created a component for multiple routes

---
+++ src/routes/index.js
@@ -0,0 +1,27 @@
+import React from "react";
+import { Route, Switch } from "react-router-dom";
+import Home from "../pages/Home";
+import Category1 from "../pages/Category1";
+import Category2 from "../pages/Category2";
+import Category3 from "../pages/Category3";
+import styled from "styled-components";
+
+const Routes = () => {
+  return (
+    <Div>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/category01" component={Category1} />
+        <Route exact path="/category02" component={Category2} />
+        <Route exact path="/category03" component={Category3} />
+      </Switch>
+    </Div>
+  );
+};
+
+export default Routes;
+
+const Div = styled.div`
+  background: lightgray;
+  flex: 1;
+`;
