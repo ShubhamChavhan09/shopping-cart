9eb2608cdc9852a917a50638fb17a5ffc6153d5a
Shubham <shubh.chavhan00@gmail.com>
2021-11-30 18:58:37 +0530

    Created reducer function with initial state and action type for adding and removing item

---
+++ src/reducers/index.js
@@ -0,0 +1,45 @@
+import faker from "faker";
+faker.seed(99);
+const product1 = [...Array(10)].map(() => ({
+  id: faker.datatype.uuid(),
+  name: faker.commerce.productName(),
+  price: faker.commerce.price(),
+  image: faker.random.image(),
+}));
+const product2 = [...Array(10)].map(() => ({
+  id: faker.datatype.uuid(),
+  name: faker.commerce.productName(),
+  price: faker.commerce.price(),
+  image: faker.random.image(),
+}));
+const product3 = [...Array(10)].map(() => ({
+  id: faker.datatype.uuid(),
+  name: faker.commerce.productName(),
+  price: faker.commerce.price(),
+  image: faker.random.image(),
+}));
+
+// eslint-disable-next-line
+const initialState = {
+  product1: product1,
+  product2: product2,
+  product3: product3,
+  cart: [],
+};
+// eslint-disable-next-line
+function reducer(state, action) {
+  switch (action.type) {
+    case "ADD_TO_CART":
+      return { ...state, cart: [...state.cart, { ...action.payload, qty: 1 }] };
+    case "REMOVE_FROM_CART":
+      return {
+        ...state,
+        cart: state.cart.filter((c) => c.id !== action.payload.id),
+      };
+
+    default:
+      return state;
+  }
+}
+
+export { initialState, reducer };
