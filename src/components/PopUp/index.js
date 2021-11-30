eb70c38d39217ae1186735dfe08c90dd11ebd92c
Shubham <shubh.chavhan00@gmail.com>
2021-11-30 20:49:02 +0530

    Added a popup component for the cart

---
+++ src/components/PopUp/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const PopUp = () => {
+  return (
+    <div>
+      <h1>Popup Cart</h1>
+    </div>
+  );
+};
+
+export default PopUp;
