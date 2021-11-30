409f384c5da040d7cf0efff46b7fe8ea9b50f024
Shubham <shubh.chavhan00@gmail.com>
2021-11-30 19:38:21 +0530

    Declared state and dispatch function from useStore and useDispatch

---
+++ src/components/SingleProduct.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { useStore, useDispatch } from "../context";
+
+const SingleProduct = ({ item }) => {
+  const state = useStore();
+  const dispatch = useDispatch();
+
+  return <div></div>;
+};
+
+export default SingleProduct;
