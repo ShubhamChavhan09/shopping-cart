1a710a60ab0f89642cd711e76b7650fe65eb0911
Shubham <shubh.chavhan00@gmail.com>
2021-11-30 19:45:32 +0530

    Added image, name, cost and button for each item

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -5,7 +5,33 @@ const SingleProduct = ({ item }) => {
   const state = useStore();
   const dispatch = useDispatch();
 
-  return <div></div>;
+  return (
+    <div>
+      <img src={item.image} alt={item.name} />
+      <p>{item.name}</p>
+      <p>₹{item.price}</p>
+
+      {/* if specific item is not in the cart the it will show Add to cart button or else remove from cart */}
+
+      {state.cart.some((p) => p.id === item.id) ? (
+        <button
+          onClick={() => {
+            dispatch({ type: "REMOVE_FROM_CART", payload: item });
+          }}
+        >
+          Remove from Cart
+        </button>
+      ) : (
+        <button
+          onClick={() => {
+            dispatch({ type: "ADD_TO_CART", payload: item });
+          }}
+        >
+          Add to Cart
+        </button>
+      )}
+    </div>
+  );
 };
 
 export default SingleProduct;
