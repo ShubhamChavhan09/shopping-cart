135be3319ba498a449a528db5040a2c89976be8c
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 14:26:50 +0530

    Added Inter font family to the app

--- src/theme/globalStyle.js
+++ src/theme/globalStyle.js
@@ -8,8 +8,9 @@ const GlobalStyle = createGlobalStyle`
 body{
     margin: 0;
     padding: 0;
+    font-family: 'Inter', sans-serif;
+    background: #F2F2F2;
 }
-
 `;
 
 export default GlobalStyle;
