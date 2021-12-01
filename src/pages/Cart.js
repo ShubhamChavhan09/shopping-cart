21cf10402e35cbcd90f0ff3e2f8e527280c770e0
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 12:26:11 +0530

    Styled Cart page to show Cart item and total price using useState and useEffect

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,14 +1,53 @@
-import React from "react";
-import { useStore } from "../context";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useStore } from "../context";
+import styled from "styled-components";
 
 const Cart = () => {
   const state = useStore();
 
+  console.log(state);
+
+  const dispatch = useDispatch();
+
+  const [total, setTotal] = useState("");
+
+  useEffect(() => {
+    setTotal(state.cart.reduce((acc, curr) => acc + Number(curr.price), 0));
+  }, [state.cart]);
+
   return (
-    <div>
-      <h1>Cart Page</h1>
-    </div>
+    <Container>
+      <List>
+        {state.cart.map((item) => (
+          <span key={item.id}>{item.name}</span>
+        ))}
+      </List>
+      <Summary>
+        <span>Subtotal ({state.cart.length}) items </span>
+        <span> Total : ₹{total} </span>
+      </Summary>
+    </Container>
   );
 };
 
 export default Cart;
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const Summary = styled.div`
+  width: 250px;
+  height: 100vh;
+  background: salmon;
+`;
+
+const List = styled.div`
+  flex: 1;
+  background: lightgray;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-basis: 100%;
+  flex-direction: column;
+`;
