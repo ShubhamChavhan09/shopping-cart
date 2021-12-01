2bf9e34e482f531679b73f623f21d53e0dd00f3e
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 21:15:49 +0530

    Added faker package to get random data for the items and render it on the page

--- src/pages/Category1.js
+++ src/pages/Category1.js
@@ -1,17 +1,33 @@
 import React from "react";
 import SingleProduct from "../components/SingleProduct";
-import { useStore } from "../context";
+import styled from "styled-components";
+import faker from "faker";
 
 const Category1 = () => {
-  const { product1 } = useStore();
+  faker.seed(99);
+  const product = [...Array(16)].map(() => ({
+    id: faker.datatype.uuid(),
+    name: faker.commerce.productName(),
+    price: faker.commerce.price(),
+    image: faker.random.image(),
+  }));
 
   return (
-    <div>
-      {product1.map((item) => (
+    <Wrapper>
+      {product.map((item) => (
         <SingleProduct item={item} key={item.id} />
       ))}
-    </div>
+    </Wrapper>
   );
 };
 
 export default Category1;
+
+const Wrapper = styled.div`
+  padding: 2rem;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1.5rem;
+  justify-content: center;
+  align-items: center;
+`;
