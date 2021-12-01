79075cf5aa4ad7350964cf0627b297a529981381
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 21:17:13 +0530

    Removed faker from the initialState

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,29 +1,5 @@
-import faker from "faker";
-faker.seed(99);
-const product1 = [...Array(10)].map(() => ({
-  id: faker.datatype.uuid(),
-  name: faker.commerce.productName(),
-  price: faker.commerce.price(),
-  image: faker.random.image(),
-}));
-const product2 = [...Array(10)].map(() => ({
-  id: faker.datatype.uuid(),
-  name: faker.commerce.productName(),
-  price: faker.commerce.price(),
-  image: faker.random.image(),
-}));
-const product3 = [...Array(10)].map(() => ({
-  id: faker.datatype.uuid(),
-  name: faker.commerce.productName(),
-  price: faker.commerce.price(),
-  image: faker.random.image(),
-}));
-
 // eslint-disable-next-line
 const initialState = {
-  product1: product1,
-  product2: product2,
-  product3: product3,
   cart: [],
 };
 // eslint-disable-next-line
