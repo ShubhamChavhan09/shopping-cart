c0581f90c8fe6a563a2dc6a7720d1676d61f017a
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 19:53:13 +0530

    Kept only a single route for category

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,16 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Home from "../pages/Home";
-import Category1 from "../pages/Category1";
-import Category2 from "../pages/Category2";
-import Category3 from "../pages/Category3";
 import styled from "styled-components";
 import Cart from "../pages/Cart";
+import Category1 from "../pages/Category1";
 
 const Routes = () => {
   return (
     <Div>
       <Switch>
         <Route exact path="/" component={Home} />
-        <Route exact path="/category01" component={Category1} />
-        <Route exact path="/category02" component={Category2} />
-        <Route exact path="/category03" component={Category3} />
+        <Route path="/categories/:id" component={Category1} />
         <Route exact path="/cart" component={Cart} />
       </Switch>
     </Div>
@@ -24,6 +20,5 @@ const Routes = () => {
 export default Routes;
 
 const Div = styled.div`
-  background: lightgray;
   flex: 1;
 `;
