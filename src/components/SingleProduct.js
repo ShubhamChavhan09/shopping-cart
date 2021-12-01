955555b9b7a12f2f11471b05a83a1a6bf47e5a12
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 14:50:55 +0530

    Added added styling to the single product page

--- src/components/SingleProduct.js
+++ src/components/SingleProduct.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { useStore, useDispatch } from "../context";
+import styled from "styled-components";
 
 const SingleProduct = ({ item }) => {
   const state = useStore();
   const dispatch = useDispatch();
 
   return (
-    <div>
+    <Container>
       <img src={item.image} alt={item.name} />
       <p>{item.name}</p>
       <p>₹{item.price}</p>
-
       {/* if specific item is not in the cart the it will show Add to cart button or else remove from cart */}
 
       {state.cart.some((p) => p.id === item.id) ? (
@@ -30,8 +30,28 @@ const SingleProduct = ({ item }) => {
           Add to Cart
         </button>
       )}
-    </div>
+    </Container>
   );
 };
 
 export default SingleProduct;
+
+const Container = styled.div`
+  border: 1px solid #2f3640;
+  box-shadow: 0 0 10px #2f3640;
+  margin: 0 auto;
+  text-align: center;
+  border-radius: 10px;
+  overflow: hidden;
+
+  img {
+    width: 200px;
+  }
+
+  button {
+    cursor: pointer;
+    width: 90%;
+    padding: 5px 0;
+    margin-bottom: 1rem;
+  }
+`;
