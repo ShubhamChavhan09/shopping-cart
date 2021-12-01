3a7f7d380e333efe0a9862a94857046e9dc564ac
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 21:13:08 +0530

    Added links on the sidebar and added styling to it

--- src/components/SideBar/index.js
+++ src/components/SideBar/index.js
@@ -6,9 +6,11 @@ const SideBar = () => {
   return (
     <>
       <Side>
-        <LinkItem to="/category01">Category 1</LinkItem>
-        <LinkItem to="/category02">Category 2</LinkItem>
-        <LinkItem to="/category03">Category 3</LinkItem>
+        <LinkItem to="/">Home</LinkItem>
+
+        <LinkItem to="/categories/1">Category 1</LinkItem>
+
+        <LinkItem to="/cart">Cart</LinkItem>
       </Side>
     </>
   );
@@ -17,19 +19,36 @@ const SideBar = () => {
 export default SideBar;
 
 const Side = styled.div`
-  background: salmon;
+  background: #f5f6fa;
+  box-shadow: 0 5px 16px rgba(0, 0, 0, 0.5);
+  border-radius: 10px;
   width: 250px;
   min-height: 92vh;
   display: flex;
+  justify-content: flex-start;
   flex-direction: column;
-  padding: 2rem 1rem;
+  margin-left: 0.5rem;
+  padding: 1rem;
 `;
 
 const LinkItem = styled(Link)`
   text-decoration: none;
   color: black;
-  border: 1px solid;
-  font-family: Sans-serif;
+  // border: 1px solid;
+  background: salmon;
+  border-radius: 8px;
   padding: 1rem;
-  margin-bottom: 1rem;
+  margin: 2rem 0;
+`;
+
+const Div = styled.div`
+  li {
+    margin-bottom: 2.5rem;
+  }
+  p {
+    margin-bottom: 1.5rem;
+    // background: salmon;
+    // border-radius: 8px;
+    // padding: 1rem;
+  }
 `;
