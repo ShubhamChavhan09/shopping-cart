8b6f795a26ee069e9bf5703d2ee9204ed701774f
Shubham <shubh.chavhan00@gmail.com>
2021-12-01 15:07:27 +0530

    Added change quantity

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -36,7 +36,13 @@ function reducer(state, action) {
         ...state,
         cart: state.cart.filter((c) => c.id !== action.payload.id),
       };
-
+    case "CHANGE_CART_QTY":
+      return {
+        ...state,
+        cart: state.cart.filter((c) =>
+          c.id === action.payload.id ? (c.qty = action.payload.qty) : c.qty
+        ),
+      };
     default:
       return state;
   }
